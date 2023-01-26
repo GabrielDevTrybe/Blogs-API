563d09dbae1c2baf0ba80063998af0fb921e6ca2
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2023-01-26 19:49:28 -0300

    requisito 01 feito

---
+++ src/migrations/20230126222822-create-blog-posts.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+
+      title: {
+        type: Sequelize.STRING
+      },
+
+      content: {
+        type: Sequelize.STRING
+      },
+
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+
+        primaryKey: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      },
+
+      created_at: {
+        field: 'published',
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        field: 'updated',
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+
+
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.dropTable('blog_posts');
+
+  }
+};
